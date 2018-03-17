8f8d0ac3d0a99f1f61fb8282fdb909778dd20d7d
alizorlu <azorlua@gmail.com>
2018-03-17 20:58:58 +0300

    Add All Files

---
+++ 0-1 Normalization/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace _0_1_Normalization
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listView1.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listView1.ForeColor = System.Drawing.Color.White;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(592, 613);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Height Data";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "0-1 Normalization";
+            this.columnHeader2.Width = 150;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 634);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(559, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start Normalization";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "0-5 Normalization";
+            this.columnHeader3.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 697);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Normalization";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ 0-1 Normalization/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0_1_Normalization
+{
+    
+    public partial class Form1 : Form
+    {
+        public List<HeightData> heightData = new List<HeightData>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnActivated(EventArgs e)
+        {
+            //Using FakeData Plugin
+            for (int i = 0; i < 50; i++)
+            {
+                heightData.Add(new HeightData
+                {
+                    Data =(double)FakeData.NumberData.GetNumber(110, 220)
+                });
+            }
+            //randomly upload 50 data add heightData list
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double maxHeight = heightData.Max(t=>t.Data);
+            double minHeight = heightData.Min(s => s.Data);
+            foreach (var item in heightData.ToList())
+            {
+                
+                HeightData newData = new HeightData();
+                newData.Data = item.Data;
+                newData.ZeroOneNormalizationData = double.Parse(string.Format("{0:0.000}", (((newData.Data) - minHeight) / (maxHeight - minHeight))));
+                newData.ZeroFiveNormalizationData = newData.ZeroOneNormalizationData
+                    * 5 - 0 + 0;
+
+                heightData.Remove(item);//old data remove(missing 01 normalization data)
+                heightData.Add(newData);//new data add heightData list
+                ListViewItem listView = new ListViewItem();
+                listView.Text = newData.Data.ToString();
+                listView.SubItems.Add(newData.ZeroOneNormalizationData.ToString("0.000###"));
+                listView.SubItems.Add(newData.ZeroFiveNormalizationData.ToString("0.000###"));
+                listView1.Items.Add(listView);
+            }
+        }
+    }
+    public class HeightData
+    {
+        public double Data { get; set; }//Height data
+        public double ZeroOneNormalizationData { get; set; }//0-1 normalization data
+        public double ZeroFiveNormalizationData { get; set; }//0-5 normalization data
+    }
+}
